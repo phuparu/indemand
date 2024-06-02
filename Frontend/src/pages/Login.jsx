fcec85bc883e4fb946ad8ba267dc121a06850cac
phuparu <phumiphat.140@gmail.com>
2024-06-03 02:10:38 +0700

    Update Login.jsx

--- Frontend/src/pages/Login.jsx
+++ Frontend/src/pages/Login.jsx
@@ -1,63 +1,125 @@
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState, useContext } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import { BASE_URL } from "../config";
+import { toast } from "react-toastify";
+import { authContext } from "../context/authController.jsx";
 
+import { mockLoginResponse } from "../assets/data/mocklogin.js";
 const Login = () => {
-
   const [formData, setFormData] = useState({
-    email:'',
-    password:'',
-  })
+    email: "",
+    password: "",
+  });
 
-  const handleInputChange = e => {
-    setFormData({ ...formData, [e.target.name]:e.target.value});
-  }
+  const handleInputChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+  const { dispatch } = useContext(authContext);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    try {
+      const result = mockLoginResponse;
+
+      // Simulate successful response
+      console.log(result, "login data");
+
+      toast.success(result.message);
+      navigate("/home");
+    } catch (err) {
+      toast.error("Failed to login. Please try again.");
+    }
+    /* setLoading(true); */
+    /* try {
+      const res = await fetch("${BASE_URL}/auth/login", {
+        method: "post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const result = await res.json();
+
+      if (!res.ok) {
+        throw new Error(result.message);
+      }
+
+      dispatch({
+        type: "LOGIN_SUCCESS",
+        payload: {
+          user: result.data,
+          token: result.token,
+          role: result.role,
+        },
+      });
+
+      console.log(result, "login data");
+
+      setLoading(false);
+      toast.success(result.message);
+      navigate("./home");
+    } catch (err) {
+      toast.error(err.message);
+      setLoading(false);
+    } */
+  };
 
   return (
-    <section className='px-5 lg:px-0'>
-      <div className='w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10'>
-        <h3 className='text-headingColor text-[22px] leading-9 font-bold mb-10'>
-          Hello! <span className='text-primaryColor'>Welcome</span> Back 👋
+    <section className="px-5 lg:px-0">
+      <div className="w-full max-w-[570px] mx-auto rounded-lg shadow-md md:p-10">
+        <h3 className="text-headingColor text-[22px] leading-9 font-bold mb-10">
+          Hello! <span className="text-primaryColor">Welcome</span> Back 👋
         </h3>
 
-        <form className='py-4 md:py-0'>
-          <div className='mb-5'>
-            <input type="email" placeholder='Enter Your Email' name='email'
-            value={formData.email} onChange={handleInputChange}
-            className='w-full px-4 py-3 border-b border-solid border-[#0066ff61]
+        <form className="py-4 md:py-0" onSubmit={submitHandler}>
+          <div className="mb-5">
+            <input
+              type="email"
+              placeholder="Enter Your Email"
+              name="email"
+              value={formData.email}
+              onChange={handleInputChange}
+              className="w-full px-4 py-3 border-b border-solid border-[#0066ff61]
             focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor
-            placeholder:text-textColor rounded-md cursor-pointer'
-            required/>
+            placeholder:text-textColor rounded-md cursor-pointer"
+              required
+            />
           </div>
 
-          <div className='mb-5'>
-            <input type="password" placeholder='Password' name='password'
-            value={formData.password} onChange={handleInputChange}
-            className='w-full px-4 py-3 border-b border-solid border-[#0066ff61]
+          <div className="mb-5">
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleInputChange}
+              className="w-full px-4 py-3 border-b border-solid border-[#0066ff61]
             focus:outline-none focus:border-b-primaryColor text-[22px] leading-7 text-headingColor
-            placeholder:text-textColor rounded-md cursor-pointer'
-            required/>
+            placeholder:text-textColor rounded-md cursor-pointer"
+              required
+            />
           </div>
 
-          <div className='mt-7'>
-            <button 
-            type='submit'
-            className='w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3'
+          <div className="mt-7">
+            <button
+              type="submit"
+              className="w-full bg-primaryColor text-white text-[18px] leading-[30px] rounded-lg px-4 py-3"
             >
               Login
             </button>
           </div>
 
-          <p className='mt-5 text-textColor text-center'>
-            Don&apos;t have an account? 
-            <Link to='/register' className='text-primaryColor font-medium ml-1'>
+          <p className="mt-5 text-textColor text-center">
+            Don&apos;t have an account?
+            <Link to="/register" className="text-primaryColor font-medium ml-1">
               Register
             </Link>
           </p>
-
         </form>
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
