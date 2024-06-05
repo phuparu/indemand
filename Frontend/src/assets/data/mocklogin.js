46b428c57d191e7b036a5e5b031951cac7a8fe79
phuparu <phumiphat.140@gmail.com>
2024-06-05 20:42:27 +0700

    Mock

---
+++ Frontend/src/assets/data/mocklogin.js
@@ -0,0 +1,12 @@
+// mockData.js
+export const mockLoginResponse = {
+    success: true,
+    message: "Login successful",
+    data: {
+      userId: 123,
+      username: "exampleUser",
+      // Add more user data as needed
+    },
+    token: "mockToken123", // Mock token for authentication
+  };
+  
---
+++ Frontend/src/assets/data/mockuser.js
@@ -0,0 +1,13 @@
+// mockuser.js
+export const users = [
+    {
+      id: 1,
+      fullname: "John Doe",
+      email: "john@example.com",
+      password: "password123",
+      gender: "male",
+      role: "student",
+    },
+    // Add more mock users as needed
+  ];
+  
