4170a47d0f2252afecca00cbe099ade5ac9433f1
phuparu <phumiphat.140@gmail.com>
2024-06-05 22:54:34 +0700

    Update Router.jsx

--- Frontend/src/routes/Router.jsx
+++ Frontend/src/routes/Router.jsx
@@ -1,25 +1,28 @@
-import React from 'react'
-import Home from '../pages/Home'
-import Course from '../pages/Course/Course'
-import CourseDetail from '../pages/Course/CourseDetail'
-import Login from '../pages/Login'
-import Signup from '../pages/Signup'
-import Contact from '../pages/Contact'
+import React from "react";
+import Home from "../pages/Home";
+import Course from "../pages/Course/Course";
+import CourseDetail from "../pages/Course/CouseDetail";
+import CourseDetail1 from "../pages/Course/CourseDetailSci";
+import CourseDetail2 from "../pages/Course/CourseDetailMath";
+import CourseDetail3 from "../pages/Course/CourseDetailEng";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import Contact from "../pages/Contact";
 
-import { Routes ,Route } from 'react-router-dom'
+import { Routes, Route } from "react-router-dom";
 
 const Router = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/home" element={<Home/>} />
-      <Route path="/course" element={<Course/>} />
-      <Route path="/coursedetail" element={<CourseDetail/>} />
-      <Route path="/login" element={<Login/>} />
-      <Route path="/register" element={<Signup/>} />
-      <Route path="/contact" element={<Contact/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/home" element={<Home />} />
+      <Route path="/course" element={<Course />} />
+      <Route path="/coursedetail/:id" element={<CourseDetail />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Signup />} />
+      <Route path="/contact" element={<Contact />} />
     </Routes>
-  )
-}
+  );
+};
 
-export default Router
+export default Router;
