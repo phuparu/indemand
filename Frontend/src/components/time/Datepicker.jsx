d661e6ed84a79fb24ae84dbcde4eb1809a7b3614
phuparu <phumiphat.140@gmail.com>
2024-06-06 17:49:48 +0700

    Create Datepicker.jsx
    DatePicker components

---
+++ Frontend/src/components/time/Datepicker.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+
+const Datepicker = ({ className, onDateChange }) => {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+    onDateChange(date);
+  };
+
+  return (
+    <div>
+      <DatePicker
+        className={`form__input ${className}`}
+        selected={selectedDate}
+        onChange={handleDateChange}
+        dateFormat="MM/dd/yyyy; hh:mm aa"
+        showTimeSelect
+        timeIntervals={30}
+        timeFormat="hh:mm aa"
+      />
+    </div>
+  );
+};
+
+export default Datepicker;
