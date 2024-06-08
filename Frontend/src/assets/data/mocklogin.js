40aec9a1ed1ebc413e49b475191b686962842fc5
phuparu <phumiphat.140@gmail.com>
2024-06-08 22:30:57 +0700

    Update mocklogin.js

--- Frontend/src/assets/data/mocklogin.js
+++ Frontend/src/assets/data/mocklogin.js
@@ -1,13 +1,27 @@
-// mockData.js
-export const mockLoginResponse = {
-    success: true,
-    message: "Login successful",
+// mocklogin.js
+export const mockLoginResponses = {
+  user: {
     data: {
-      fullname: "Phu",
-      password: 123,
-      email: "example@gmail.com",
-      role:'student',
+      user: { fullname: "Josh", email: "user@example.com" },
+      token: "user-token",
+      role: "student",
     },
-    token: "mockToken123", // Mock token for authentication
-  };
-  
+    message: "User logged in successfully",
+  },
+  tutor: {
+    data: {
+      user: { fullname: "Jane Tutor", email: "tutor@example.com" },
+      token: "tutor-token",
+      role: "tutor",
+    },
+    message: "Tutor logged in successfully",
+  },
+  admin: {
+    data: {
+      user: { fullname: "Admin", email: "admin@example.com" },
+      token: "admin-token",
+      role: "admin",
+    },
+    message: "Admin logged in successfully",
+  },
+};
